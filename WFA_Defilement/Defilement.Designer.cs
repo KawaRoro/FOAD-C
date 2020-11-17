dba2c5e3976b10fedcc1026994ece7aa246f01a9
KawaRoro <webmaster.benjamin@gmail.com>
2020-11-17 14:59:57 +0100

    BS: Create Exo Defilement #1
    BS: Create Exo Defilement #1

---
+++ WFA_Defilement/Defilement.Designer.cs
@@ -0,0 +1,229 @@
+﻿
+namespace WFA_Defilement
+{
+    partial class Defilement
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.hScrollBarRed = new System.Windows.Forms.HScrollBar();
+            this.hScrollBarGreen = new System.Windows.Forms.HScrollBar();
+            this.hScrollBarBlue = new System.Windows.Forms.HScrollBar();
+            this.numericUpDownRed = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownGreen = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownBlue = new System.Windows.Forms.NumericUpDown();
+            this.textBoxRed = new System.Windows.Forms.TextBox();
+            this.textBoxGreen = new System.Windows.Forms.TextBox();
+            this.textBoxBlue = new System.Windows.Forms.TextBox();
+            this.textBoxColor = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownGreen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownBlue)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Rouge";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Vert";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Bleu";
+            // 
+            // hScrollBarRed
+            // 
+            this.hScrollBarRed.LargeChange = 1;
+            this.hScrollBarRed.Location = new System.Drawing.Point(90, 50);
+            this.hScrollBarRed.Maximum = 255;
+            this.hScrollBarRed.Name = "hScrollBarRed";
+            this.hScrollBarRed.Size = new System.Drawing.Size(255, 17);
+            this.hScrollBarRed.TabIndex = 3;
+            this.hScrollBarRed.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBarRed_Scroll);
+            // 
+            // hScrollBarGreen
+            // 
+            this.hScrollBarGreen.LargeChange = 1;
+            this.hScrollBarGreen.Location = new System.Drawing.Point(90, 100);
+            this.hScrollBarGreen.Maximum = 255;
+            this.hScrollBarGreen.Name = "hScrollBarGreen";
+            this.hScrollBarGreen.Size = new System.Drawing.Size(255, 17);
+            this.hScrollBarGreen.TabIndex = 4;
+            this.hScrollBarGreen.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBarGreen_Scroll);
+            // 
+            // hScrollBarBlue
+            // 
+            this.hScrollBarBlue.LargeChange = 1;
+            this.hScrollBarBlue.Location = new System.Drawing.Point(90, 150);
+            this.hScrollBarBlue.Maximum = 255;
+            this.hScrollBarBlue.Name = "hScrollBarBlue";
+            this.hScrollBarBlue.Size = new System.Drawing.Size(255, 17);
+            this.hScrollBarBlue.TabIndex = 5;
+            this.hScrollBarBlue.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBarBlue_Scroll);
+            // 
+            // numericUpDownRed
+            // 
+            this.numericUpDownRed.Location = new System.Drawing.Point(364, 50);
+            this.numericUpDownRed.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDownRed.Name = "numericUpDownRed";
+            this.numericUpDownRed.Size = new System.Drawing.Size(60, 20);
+            this.numericUpDownRed.TabIndex = 6;
+            this.numericUpDownRed.ValueChanged += new System.EventHandler(this.numericUpDownRed_ValueChanged);
+            // 
+            // numericUpDownGreen
+            // 
+            this.numericUpDownGreen.Location = new System.Drawing.Point(364, 100);
+            this.numericUpDownGreen.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDownGreen.Name = "numericUpDownGreen";
+            this.numericUpDownGreen.Size = new System.Drawing.Size(60, 20);
+            this.numericUpDownGreen.TabIndex = 7;
+            this.numericUpDownGreen.ValueChanged += new System.EventHandler(this.numericUpDownGreen_ValueChanged);
+            // 
+            // numericUpDownBlue
+            // 
+            this.numericUpDownBlue.Location = new System.Drawing.Point(364, 150);
+            this.numericUpDownBlue.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDownBlue.Name = "numericUpDownBlue";
+            this.numericUpDownBlue.Size = new System.Drawing.Size(60, 20);
+            this.numericUpDownBlue.TabIndex = 8;
+            this.numericUpDownBlue.ValueChanged += new System.EventHandler(this.numericUpDownBlue_ValueChanged);
+            // 
+            // textBoxRed
+            // 
+            this.textBoxRed.BackColor = System.Drawing.Color.Red;
+            this.textBoxRed.Enabled = false;
+            this.textBoxRed.Location = new System.Drawing.Point(440, 50);
+            this.textBoxRed.Name = "textBoxRed";
+            this.textBoxRed.Size = new System.Drawing.Size(100, 20);
+            this.textBoxRed.TabIndex = 9;
+            // 
+            // textBoxGreen
+            // 
+            this.textBoxGreen.BackColor = System.Drawing.Color.Green;
+            this.textBoxGreen.Enabled = false;
+            this.textBoxGreen.Location = new System.Drawing.Point(440, 100);
+            this.textBoxGreen.Name = "textBoxGreen";
+            this.textBoxGreen.Size = new System.Drawing.Size(100, 20);
+            this.textBoxGreen.TabIndex = 10;
+            // 
+            // textBoxBlue
+            // 
+            this.textBoxBlue.BackColor = System.Drawing.Color.Blue;
+            this.textBoxBlue.Enabled = false;
+            this.textBoxBlue.Location = new System.Drawing.Point(440, 150);
+            this.textBoxBlue.Name = "textBoxBlue";
+            this.textBoxBlue.Size = new System.Drawing.Size(100, 20);
+            this.textBoxBlue.TabIndex = 11;
+            // 
+            // textBoxColor
+            // 
+            this.textBoxColor.BackColor = System.Drawing.Color.Black;
+            this.textBoxColor.Location = new System.Drawing.Point(40, 200);
+            this.textBoxColor.Multiline = true;
+            this.textBoxColor.Name = "textBoxColor";
+            this.textBoxColor.Size = new System.Drawing.Size(500, 60);
+            this.textBoxColor.TabIndex = 12;
+            // 
+            // Defilement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 304);
+            this.Controls.Add(this.textBoxColor);
+            this.Controls.Add(this.textBoxBlue);
+            this.Controls.Add(this.textBoxGreen);
+            this.Controls.Add(this.textBoxRed);
+            this.Controls.Add(this.numericUpDownBlue);
+            this.Controls.Add(this.numericUpDownGreen);
+            this.Controls.Add(this.numericUpDownRed);
+            this.Controls.Add(this.hScrollBarBlue);
+            this.Controls.Add(this.hScrollBarGreen);
+            this.Controls.Add(this.hScrollBarRed);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Defilement";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Configurez votre couleur RVB";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownGreen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownBlue)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.HScrollBar hScrollBarRed;
+        private System.Windows.Forms.HScrollBar hScrollBarGreen;
+        private System.Windows.Forms.HScrollBar hScrollBarBlue;
+        private System.Windows.Forms.NumericUpDown numericUpDownRed;
+        private System.Windows.Forms.NumericUpDown numericUpDownGreen;
+        private System.Windows.Forms.NumericUpDown numericUpDownBlue;
+        private System.Windows.Forms.TextBox textBoxRed;
+        private System.Windows.Forms.TextBox textBoxGreen;
+        private System.Windows.Forms.TextBox textBoxBlue;
+        private System.Windows.Forms.TextBox textBoxColor;
+    }
+}
+
---
+++ WFA_Defilement/Defilement.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA_Defilement
+{
+    public partial class Defilement : Form
+    {
+        private System.Drawing.Color colorZone;
+
+        public Defilement()
+        {
+            InitializeComponent();
+            colorZone = Color.FromArgb(0, 0, 0);
+        }
+        public Defilement(Color _colorValue)
+        {
+            InitializeComponent();
+            colorZone = _colorValue;
+            UpdateColorZone();
+        }
+
+        /// <summary>
+        /// Update zone color
+        /// </summary>
+        public void UpdateColorZone()
+        {
+            this.hScrollBarRed.Value = colorZone.R;
+            this.numericUpDownRed.Value = colorZone.R;
+            this.textBoxRed.BackColor = Color.FromArgb(colorZone.R, 0, 0);
+
+            this.hScrollBarGreen.Value = colorZone.G;
+            this.numericUpDownGreen.Value = colorZone.G;
+            this.textBoxGreen.BackColor = Color.FromArgb(0, colorZone.G, 0);
+
+            this.hScrollBarBlue.Value = colorZone.B;
+            this.numericUpDownBlue.Value = colorZone.B;
+            this.textBoxBlue.BackColor = Color.FromArgb(0, 0, colorZone.B);
+
+            textBoxColor.BackColor = colorZone;
+        }
+
+        private void hScrollBarRed_Scroll(object sender, ScrollEventArgs e)
+        {
+            colorZone = Color.FromArgb(hScrollBarRed.Value, colorZone.G, colorZone.B);
+            UpdateColorZone();
+        }
+
+        private void hScrollBarGreen_Scroll(object sender, ScrollEventArgs e)
+        {
+            colorZone = Color.FromArgb(colorZone.R, hScrollBarGreen.Value, colorZone.B);
+            UpdateColorZone();
+        }
+
+        private void hScrollBarBlue_Scroll(object sender, ScrollEventArgs e)
+        {
+            colorZone = Color.FromArgb(colorZone.R, colorZone.G, hScrollBarBlue.Value);
+            UpdateColorZone();
+        }
+
+        private void numericUpDownRed_ValueChanged(object sender, EventArgs e)
+        {
+            colorZone = Color.FromArgb((int)numericUpDownRed.Value, colorZone.G, colorZone.B);
+            UpdateColorZone();
+        }
+
+        private void numericUpDownGreen_ValueChanged(object sender, EventArgs e)
+        {
+            colorZone = Color.FromArgb(colorZone.R, (int)numericUpDownGreen.Value, colorZone.B);
+            UpdateColorZone();
+        }
+
+        private void numericUpDownBlue_ValueChanged(object sender, EventArgs e)
+        {
+            colorZone = Color.FromArgb(colorZone.R, colorZone.G, (int)numericUpDownBlue.Value);
+            UpdateColorZone();
+        }
+
+    }
+}
